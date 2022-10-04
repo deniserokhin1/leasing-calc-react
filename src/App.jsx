8cd55ea746984b5e59c88abc4c373e04d75b909c
Denis <oterohina@gmail.com>
2022-10-04 19:13:24 +0400

    Add second and third inputs

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,47 @@ import React from 'react';
 function App() {
   return (
     <div className="container">
-      <h1>Рассчитайте стоимость автомобиля в лизинг</h1>
+      <h1 className="title">Рассчитайте стоимость автомобиля в лизинг</h1>
       <div className="content">
         <div className="column-input">
           <div className="content__input price-car">
             <p className="text">Стоимость автомобиля</p>
             <div className="block-input">
               <input className="input-number" type="number" />
+              <span className="units">₽</span>
+            </div>
+            <div className="block__slider">
+              <div className="slider">
+                <div className="slider__progress"></div>
+              </div>
+            </div>
+            <div className="block-range">
+              <div className="range">
+                <input className="range__input" type="range" />
+              </div>
+            </div>
+          </div>
+          <div className="content__input price-car">
+            <p className="text">Первоначальный взнос</p>
+            <div className="block-input">
+              <input className="input-number" type="number" />
+              <span className="units">13%</span>
+            </div>
+            <div className="block__slider">
+              <div className="slider">
+                <div className="slider__progress"></div>
+              </div>
+            </div>
+            <div className="range">
+              <input className="range__input" type="range" />
+            </div>
+          </div>
+          <div className="content__input price-car">
+            <p className="text">Срок лизинга</p>
+            <div className="block-input">
+              <input className="input-number" type="number" />
               <span className="units">мес.</span>
             </div>
-
             <div className="block__slider">
               <div className="slider">
                 <div className="slider__progress"></div>
@@ -25,10 +56,19 @@ function App() {
           </div>
         </div>
         <div className="column-info">
-          <p className="info__text">Сумма договора лизинга</p>
-          <div className="info__money">
-            4467313 <span>₽</span>
+          <div className="block-info">
+            <p className="info__text">Сумма договора лизинга</p>
+            <div className="info__money">
+              4467313 <span>₽</span>
+            </div>
           </div>
+          <div className="block-info">
+            <p className="info__text">Ежемесячный платеж от</p>
+            <div className="info__money">
+              4467313 <span>₽</span>
+            </div>
+          </div>
+          <button className="btn">Оставить заявку</button>
         </div>
       </div>
     </div>
